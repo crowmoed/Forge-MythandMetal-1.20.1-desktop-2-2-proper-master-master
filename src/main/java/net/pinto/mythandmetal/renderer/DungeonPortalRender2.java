0f29629476342e8e9f919e46b2a50774f6fe23b3
crowmoed <notcristianopinto@gmail.com>
2025-03-25 01:25:18 -0400

    brtuh

--- src/main/java/net/pinto/mythandmetal/renderer/DungeonPortalRender2.java
+++ src/main/java/net/pinto/mythandmetal/renderer/DungeonPortalRender2.java
@@ -22,19 +22,6 @@ public class DungeonPortalRender2  extends  DungeonPortalRenderer<ModDungeonPort
     private static final ResourceLocation BEAM_LOCATION = new ResourceLocation("textures/entity/end_gateway_beam.png");
 
 
-    public void render(ModDungeonPortalDoorBlockEntity p_112613_, float p_112614_, PoseStack p_112615_, MultiBufferSource p_112616_, int p_112617_, int p_112618_) {
-        if (p_112613_.isSpawning() || p_112613_.isCoolingDown()) {
-            float f = p_112613_.isSpawning() ? p_112613_.getSpawnPercent(p_112614_) : p_112613_.getCooldownPercent(p_112614_);
-            double d0 = p_112613_.isSpawning() ? (double)p_112613_.getLevel().getMaxBuildHeight() : 50.0D;
-            f = Mth.sin(f * (float)Math.PI);
-            int i = Mth.floor((double)f * d0);
-            float[] afloat = p_112613_.isSpawning() ? DyeColor.MAGENTA.getTextureDiffuseColors() : DyeColor.PURPLE.getTextureDiffuseColors();
-            long j = p_112613_.getLevel().getGameTime();
-            BeaconRenderer.renderBeaconBeam(p_112615_, p_112616_, BEAM_LOCATION, p_112614_, f, j, -i, i * 2, afloat, 0.15F, 0.175F);
-        }
-
-        super.render(p_112613_, p_112614_, p_112615_, p_112616_, p_112617_, p_112618_);
-    }
 
     protected float getOffsetUp() {
         return 1.0F;
