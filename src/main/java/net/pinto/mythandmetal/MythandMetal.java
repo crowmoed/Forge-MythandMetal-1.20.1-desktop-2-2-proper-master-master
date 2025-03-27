15424277613220163765244166948021192d5350
crowmoed <notcristianopinto@gmail.com>
2025-03-26 20:55:27 -0400

    brtuh

--- src/main/java/net/pinto/mythandmetal/MythandMetal.java
+++ src/main/java/net/pinto/mythandmetal/MythandMetal.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -31,6 +32,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.pinto.mythandmetal.block.ModBlocks;
 import net.pinto.mythandmetal.block.customEntity.MyBlockEntityTypes;
+import net.pinto.mythandmetal.effects.ModParticleTypes;
 import net.pinto.mythandmetal.entity.AshenWolf.AshenWolfRenderer;
 import net.pinto.mythandmetal.entity.CustVillager;
 import net.pinto.mythandmetal.entity.ModEntites;
@@ -77,6 +79,7 @@ public class MythandMetal
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         CustVillager.register(modEventBus);
+        ModParticleTypes.registerParticleFactories(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
         ModItems.register(modEventBus);
         ModEntites.register(modEventBus);
